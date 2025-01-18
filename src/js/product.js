74aaec63a878a8006d00ee46e0a58e33806bc9e8
Eric Kodzi <kod24002@byui.edu>
2025-01-18 14:25:45 +0000

    Add item to cart broken fix

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage,getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve the existing cart array from localStorage or initialize it as an empty array
+  let cart = getLocalStorage("so-cart") || [];
+
+  // Add the new product to the cart array
+  cart.push(product);
+
+  // Save the updated cart array back to localStorage
+  setLocalStorage("so-cart", cart);
 }
-// add to cart button event handler
+
+
+// add to cart button, event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
