d91df04031fe4f462cf9c46d41e5fff2a8f0f64b
Kehinde John Omotoso <kehindejohnomotoso@gmail.com>
2025-01-26 23:31:52 +0100

    W2 Assignments

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,23 +1,30 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
+////////
+function convertToJson(response){
+  if(response.ok){
+    return response.json();
+  }else{
+    throw new Error("Bad Response Data")
   }
 }
 
-export default class ProductData {
-  constructor(category = "tents") {
+//////////
+export default class ProductData{
+  constructor(category = "tents"){
     this.category = category;
     this.path = `/json/${this.category}.json`;
   }
-  getData() {
+  ////Get Data function
+  getData(){
     return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+    .then(convertToJson)
+    .then((data) => data);
   }
-  async findProductById(id) {
+
+  ////Find Product By ID
+  async findProductById(id){
     const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    return products.find((item) => item.Id == id);
   }
 }
+////////
+////////
